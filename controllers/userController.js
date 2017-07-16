6783c139f07c06d680ebfbd7613f9894c622dd72
Uci Arahito Lubis <uarahito@yahoo.com>
2017-07-16 12:28:09 +0700

    Revert "Revert "fixed bug in userController""

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,8 +17,6 @@ methods.signUp = (req, res) => {
     newUser.save((err, data) => {
       if (err) res.json({err})
 
-      // console.log('SignUp success');
-      // console.log(data);
       res.json({
         status: true,
         data
@@ -30,61 +28,59 @@ methods.signUp = (req, res) => {
       message: 'Password length min 5 character'
     })
   }
-
-  // newUser.save((err, data) => {
-  //   if (err) res.json({err})
-  //   console.log('SignUp success');
-  //   console.log(data);;
-  //   res.send(data)
-  // })
 } //signup
 
 methods.signIn = (req, res) => {
   let pwd = req.body.password
-  User.findOne({
-    username: req.body.username
-  })
-  .then(record => {
-    if (req.body.username !== record.username) {
-        res.status(404).json({
-          status: false,
-          message: 'Please input the correct username!'
-        })
-    } else {
-      if (bCrypt.compareSync(pwd, record.password)) {
-        let token = jwt.sign({
-          id: record._id,
-          username: record.username,
-          email: record.email
-        }, process.env.SECRET_KEY, { expiresIn: '1d'})
-        // console.log('token login: '+token);
-        res.json({
-          message: 'SignIn success',
-          id: record._id,
-          username: record.username,
-          token
-        })
-      } else {
-        res.status(404).json({
-          status: false,
-          message: 'Please input the correct password!'
-        })
-      }
-    }
-  })
-  .catch(error => {
+  if (req.body.username.length === 0) {
     res.status(404).json({
       status: false,
-      message: 'Please input the correct username and password!'
+      message: 'Username is required!'
     })
-  })
+  } else {
+    User.findOne({
+      username: req.body.username
+    })
+    .then(record => {
+      if (req.body.password.length === 0) {
+        res.status(404).json({
+          status: false,
+          message: 'Password is required!'
+        })
+      } else {
+        if (bCrypt.compareSync(pwd, record.password)) {
+          let token = jwt.sign({
+            id: record._id,
+            username: record.username,
+            email: record.email
+          }, process.env.SECRET_KEY, { expiresIn: '1d'})
+          res.json({
+            message: 'SignIn success',
+            id: record._id,
+            username: record.username,
+            token
+          })
+        } else {
+          res.status(404).json({
+            status: false,
+            message: 'Please input the correct password!'
+          })
+        }
+      }
+    })
+    .catch(error => {
+      res.status(404).json({
+        status: false,
+        message: 'Please input the correct username!'
+      })
+    })
+  }
+
 } //signin biasa
 
 methods.getAllUsers = (req, res) => {
   User.find({}, (err, records) => {
     if (err) res.json({err})
-    // console.log('Data all user success');
-    // console.log(records);
     res.json(records)
   })
 } // getAllUser
@@ -92,8 +88,6 @@ methods.getAllUsers = (req, res) => {
 methods.getUserById = (req, res) => {
   User.findById(req.params.id, (err, record) => {
     if (err) res.json({err})
-    // console.log('Detail User success');
-    // console.log(record);
     res.json(record)
   })
 } //getUserById
@@ -101,8 +95,6 @@ methods.getUserById = (req, res) => {
 methods.getHomeAddressGeolocation = (req, res) => {
   User.findById(req.params.id, (err, record) => {
     if (err) res.json({err})
-    // console.log('Detail User success');
-    // console.log(record.homeAddressGeolocation);
     res.json(record.homeAddressGeolocation)
   })
 } //getHomeAddressGeolocation
@@ -110,8 +102,6 @@ methods.getHomeAddressGeolocation = (req, res) => {
 methods.getOfficeAddressGeolocation = (req, res) => {
   User.findById(req.params.id, (err, record) => {
     if (err) res.json({err})
-    // console.log('Detail User success');
-    // console.log(record);
     res.json(record.officeAddressGeolocation)
   })
 } //getOfficeAddressGeolocation
@@ -121,9 +111,6 @@ methods.editUser = (req, res) => {
     User.findById(req.params.id)
     .exec((error, response) => {
       if (error) res.json({error})
-      // console.log(response._id);
-      // console.log('Masuk gakkk: '+ req.body.name);
-      // console.log('pwd hash di editUser:  '+bCrypt.hashSync(pwdUser, saltRounds));
       User.findByIdAndUpdate({
         "_id": response._id
       }, {
@@ -145,13 +132,11 @@ methods.editUser = (req, res) => {
       .exec((err, result) => {
         if (err) res.json({err})
         res.json(result)
-        // console.log('edit user success');
       })
     })
 } //editUser
 
 methods.updateAvatarUrl = (req, res) => {
-  console.log('??????????????');
   User.findById(req.params.id, (err, record) => {
     if(err) res.json({err})
     record.avatarURL = req.body.avatar
@@ -165,8 +150,6 @@ methods.updateAvatarUrl = (req, res) => {
 methods.deleteUserById = (req, res) => {
     User.findByIdAndRemove(req.params.id, (err, record) => {
       if (err) res.json({err})
-      // console.log('Delete user success');
-      // console.log(record);
       res.json(record)
     })
 } //deleteUser
